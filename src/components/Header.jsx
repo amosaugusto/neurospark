1c07daf631fe782f0ea64d9dce13c0827c7caf01
Amos Augusto <amosaugustos@gmail.com>
2024-08-09 00:28:08 +0700

    feat: fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
-import { brainwave } from "../assets";
+import { brainwave, brainwaveSymbol } from "../assets";
 import { navigation } from "../constants";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
@@ -35,8 +35,9 @@ const Header = () => {
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src={brainwave} width={190} height={40} alt="Neurospark" />
+        <a className="flex items-center w-[12rem] xl:mr-8" href="#hero">
+          <img src={brainwaveSymbol} width={40} height={40} alt="Neurospark" />
+          <p className="flex ml-2 h5 font-bold">Neurospark</p>
         </a>
 
         <nav
