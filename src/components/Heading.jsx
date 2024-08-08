756f0bb79e1ff26c48a43e5eaf514ae0f331b419
Amos Augusto <amosaugustos@gmail.com>
2024-08-08 11:03:42 +0700

    feat: add collaboration section

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,8 +1,6 @@
-const Heading = ({ className, title, text, tag }) => {
+const Heading = ({ className, title }) => {
   return (
-    <div
-      className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}
-    >
+    <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
       {title && <h2 className="h2">{title}</h2>}
     </div>
   );
